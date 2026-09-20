15aa9ab35e7156e302f856ac23a66065c9cb0985
price-path <price-path@local>
2026-09-20 08:59:58 +0700

    config.js: khoa cong khai VAPID

--- docs/config.js
+++ docs/config.js
@@ -2,6 +2,6 @@
 // (sinh bằng venv\Scripts\python -m job.gen_vapid). Để trống = nút "Bật thông báo" bị khoá.
 // WORKER_URL trống = điện thoại hiện đoạn mã đăng ký để dán vào Secret PUSH_SUBS_FALLBACK.
 window.PP_CONFIG = {
-  VAPID_PUBLIC: "",
+  VAPID_PUBLIC: "BKovgXaJrzCbrYAEQLWnQl39OtVNzmw9ZqK-jRJhuL_7-Va0w6vd_qvB9aVcV6sdZ8g8fF2jDMSbWojcoqUkuzA",
   WORKER_URL: "",
 };
